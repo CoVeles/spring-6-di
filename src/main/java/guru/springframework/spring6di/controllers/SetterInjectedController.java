4c905478a7ac95d032709cc42bb60379712aca73
Konstantin Volosenkov <konstantin.volosenkov@sap.com>
2023-12-04 20:08:52 +0500

    Add Qualifiers

--- src/main/java/guru/springframework/spring6di/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/spring6di/controllers/SetterInjectedController.java
@@ -1,13 +1,16 @@
 package guru.springframework.spring6di.controllers;
 
 import guru.springframework.spring6di.services.GreetingService;
+import guru.springframework.spring6di.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SetterInjectedController {
-
     private GreetingService greetingService;
+
+    @Qualifier("setterGreetingBean")
     @Autowired
     public void setGreetingService(GreetingService greetingService) {
         System.out.println("Setter is utilized");
