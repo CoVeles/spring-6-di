efe2572d2e35ef08c144c53d0f0a7b57877c5dbd
Konstantin Volosenkov <konstantin.volosenkov@sap.com>
2023-12-10 14:56:33 +0500

    Add Default Profile

--- src/main/java/guru/springframework/spring6di/services/i18n/EnglishGreetingService.java
+++ src/main/java/guru/springframework/spring6di/services/i18n/EnglishGreetingService.java
@@ -3,7 +3,9 @@ package guru.springframework.spring6di.services.i18n;
 import guru.springframework.spring6di.services.GreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-@Profile("EN")
+// If you need service as default, that means if no one profile (e.g. EN) is mentioned
+@Profile({"EN", "default"})
+//@Profile("EN")
 @Service("i18NService")
 public class EnglishGreetingService implements GreetingService {
     @Override
--- src/test/java/guru/springframework/spring6di/controllers/MyControllerTest.java
+++ src/test/java/guru/springframework/spring6di/controllers/MyControllerTest.java
@@ -1,9 +1,10 @@
 package guru.springframework.spring6di.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class MyControllerTest {
 
     @Test
